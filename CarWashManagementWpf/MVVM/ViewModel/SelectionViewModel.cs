2a69eba597c2345fc4b1a194be4e9667f2447516
mykolakraievskyi <m.kraievskyi@gmail.com>
2023-11-27 13:13:19 +0200

    Records can now be added to a database

--- CarWashManagementWpf/MVVM/ViewModel/SelectionViewModel.cs
+++ CarWashManagementWpf/MVVM/ViewModel/SelectionViewModel.cs
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace CarWashManagementWpf.MVVM.ViewModel
 {
@@ -49,6 +50,57 @@ namespace CarWashManagementWpf.MVVM.ViewModel
         }
         public RelayCommand NavigateToMoneySplit { get; set; }
         public RelayCommand NavigateToWholeTable { get; set; }
+        public RelayCommand SyncWorkersCommand { get; set; }
+        public RelayCommand NextCommand { get; set; }
+        public string WorkersString { get; set; }
+
+        /// <summary>
+        /// A method that takes the chosen workers and converts them to a string (the type of parametr used in DBConnection.AddRecord())
+        /// </summary>
+        /// <remarks>
+        /// The result is saved in a WorkersString property
+        /// </remarks>
+        /// <param name="o"></param>
+        /// <returns>true, if worker(s) were chosen and the conversion is successful, false othewvise</returns>
+        bool SyncWorkersMethod(object o)
+        {
+            if (o == null)
+            {
+                MessageBox.Show("Please, choose workers", "Worker(s) was not chosen", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            } else
+            {
+                System.Collections.IList items = o as System.Collections.IList;
+                var collection = items.Cast<string>().ToList();
+                WorkersString = string.Join(", ", collection);
+                return true;
+            }
+        }
+
+        bool AddRecord()
+        {
+            if (ChosenUtility != null && string.IsNullOrEmpty(WorkersString) == false)
+            {
+                BindInstance.BindInstance.DBConnection.AddRecord(ChosenUtility.Name, WorkersString);
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Please, choose all the parameters", "Some parameters were not chosen", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+        }
+
+        void AddAndNextPage(object o)
+        {
+            if (SyncWorkersMethod(o))
+            {
+                if (AddRecord())
+                {
+                    Navigation.NavigateTo<WholeTableViewModel>();
+                }
+            }
+        }
 
         public SelectionViewModel(INavigationService navigation, IBindService bind)
         {
@@ -56,6 +108,9 @@ namespace CarWashManagementWpf.MVVM.ViewModel
             Navigation = navigation;
             NavigateToWholeTable = new RelayCommand(execute: o => Navigation.NavigateTo<WholeTableViewModel>(), canExecute: o => true);
             NavigateToMoneySplit = new RelayCommand(execute: o => Navigation.NavigateTo<MoneySplitViewModel>(), canExecute: o => true);
+            //SyncWorkersCommand = new RelayCommand(execute: o => Navigation.NavigateTo<MoneySplitViewModel>(), canExecute: o => true);
+            SyncWorkersCommand = new RelayCommand(execute: o => { SyncWorkersMethod(o); }, canExecute: o => true);
+            NextCommand = new RelayCommand(execute: o => { AddAndNextPage(o); }, canExecute: o => true);
             Workers = BindInstance.BindInstance.GetWorkers();
             utilities = new ObservableCollection<Utility>()
             {
